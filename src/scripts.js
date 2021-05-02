0e4ade93b5129c48eebf691fc3d8d47b7d037ee2
Kelly Young <kkristineyoung@gmail.com>
2021-05-01 22:40:46 -0500

    Adds ability to click 'search' and open new tab with specified siet and search results

---
+++ src/scripts.js
@@ -0,0 +1,13 @@
+// Jojoba is a placeholder. Jojoba will be replaced with a variable based on input. Hopefully. 
+function itemSearch() {
+
+  // LotionCrafter
+  window.open(
+    "https://lotioncrafter.com/search?type=product&q=jojoba*", "_blank");
+
+  // // NewDirections
+  // window.open(
+  //   "https://www.newdirectionsaromatics.com/products/advanced_search_result.php?keywords=jojoba", "_blank");
+}
+
+
