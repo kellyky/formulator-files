879753197d50cdbad79c0e3166ef4f55120f0619
Kelly Young <kkristineyoung@gmail.com>
2021-05-01 23:29:50 -0500

    Adds (failed) attempt to open multiple urls each in its own tab

--- src/scripts.js
+++ src/scripts.js
@@ -1,13 +1,14 @@
 // Jojoba is a placeholder. Jojoba will be replaced with a variable based on input. Hopefully. 
+
+const lotionCrafter = "https://lotioncrafter.com/search?type=product&q=jojoba*"
+const newDirections = "https://www.newdirectionsaromatics.com/products/advanced_search_result.php?keywords=jojoba"
+
+const stores = [newDirections, lotionCrafter]
+
 function itemSearch() {
-
-  // LotionCrafter
-  window.open(
-    "https://lotioncrafter.com/search?type=product&q=jojoba*", "_blank");
-
-  // // NewDirections
-  // window.open(
-  //   "https://www.newdirectionsaromatics.com/products/advanced_search_result.php?keywords=jojoba", "_blank");
+  stores.forEach(search => {
+    window.open(stores);
+  });
 }
 
-
+button.addEventListener("click", itemSearch);
